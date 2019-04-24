0167b890f353bf3d33d43a0849b5641356104b92
Ashwani Gupta <ashwanigupta125@gmail.com>
2019-04-24 09:34:30 +0530

    Add bills model

--- app/models.py
+++ app/models.py
@@ -1,10 +1,13 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db, jwt
+from sqlalchemy.orm import backref
 
 from datetime import datetime
 
 
 class User(db.Model):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(254), nullable=False, unique=True)
     username = db.Column(db.String(128), nullable=False)
@@ -12,7 +15,7 @@ class User(db.Model):
     registered_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return '<User {}, id={}>'.format(self.username, self.id)
+        return '<User({}, id={})>'.format(self.username, self.id)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -29,4 +32,52 @@ def get_user_id(user):
 @jwt.user_loader_callback_loader
 def load_user(id):
     return User.query.get(id)
+
+
+class Bill(db.Model):
+    __tablename__ = 'bills'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(64), nullable=False)
+    desc = db.Column(db.String(256))
+    amount = db.Column(db.Float, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def add_participant(self, participant, share, is_payer=False):
+        bd = BillDetails.query.filter_by(bill=self, user=participant)
+        # update if found
+        if bd is not None:
+            bd.share = share
+            bd.is_payer = is_payer
+        else:
+            self.bill_details.append(
+                BillDetails(bill=self, user=participant, share=share, is_payer=is_payer)
+            )
+
+    def remove_participant(self, participant):
+        bd = BillDetails.query.filter_by(bill=self, user=participant).scalar()
+        if bd is not None:
+            self.bill_details.remove(bd)
+
+    def __repr__(self):
+        return '<Bill({}, id={})>'.format(self.title, self.id)
+
+
+class BillDetails(db.Model):
+    __tablename__ = 'bill_details'
     
+    bill_id = db.Column(db.Integer, db.ForeignKey('bills.id'), nullable=False,
+        primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False,
+        primary_key=True)
+    is_payer = db.Column(db.Boolean, default=False)
+    share = db.Column(db.Float, nullable=False, default=0.0)
+
+    user = db.relationship('User', backref=backref('bill_details', 
+        cascade='all, delete-orphan'))
+    bill = db.relationship('Bill', backref=backref('bill_details',
+        cascade='all, delete-orphan'))
+
+    def __repr__(self):
+        return '<BillDetail(bill={}, user={})>'.format(self.bill_id, self.user_id)
--- splitter.py
+++ splitter.py
@@ -1,13 +1,12 @@
 from app import create_app, db, jwt
 
-
 app = create_app()
 with app.app_context(): 
     db.create_all() # TODO: refactor this
 
-from app.models import User
+from app.models import User, Bill, BillDetails
 
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User}
+    return {'db': db, 'User': User, 'Bill': Bill, 'BillDetails': BillDetails}
