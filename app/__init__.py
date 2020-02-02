25c666aaa0c0aeacc422bbc3069b85a87321a552
Ashwani Gupta <ashwanigupta125@gmail.com>
2020-02-02 19:21:20 +0530

    core: add error handlers compatible to both Flask and Flask-Restful
    Now API requests must need to be having application/json mimetype. Also, Flask-Restful default error handler is overridden to support error de-serialization

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,30 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_restful import Api
 from flask_jwt_extended import JWTManager
+import flask_restful
 
 from config import Config
-from app.exceptions import ApiException
+from app.exceptions import ApiException, ParseError
+
+
+class SplitterAPI(flask_restful.Api):
+    """ Wrapper around `flask_restful.Api` with specifics for Splitter API """
+
+    # Flask-Restful's error handling mechanism doesn't follow Flask.errorhandler specs
+    # overriding `flask_restful.Api.handle_error` to serialize all internal exceptions to JSON
+    def handle_error(self, e):
+        if isinstance(e, ApiException):
+            # for un-handled exceptions, leave them as it is for now
+            # TODO: add logging, wrap the un-handled exceptions into `ApiException`
+            return make_response(jsonify(e.to_dict()), e.error_status_code)
+        else:
+            raise
 
 
 db = SQLAlchemy()
 migrate = Migrate()
-api = Api()
+api = SplitterAPI()
 jwt = JWTManager()
 
 
@@ -27,7 +41,15 @@ def create_app(config=Config):
     from app.auth import bp as auth_bp
     app.register_blueprint(auth_bp, url_prefix='/auth')
 
-    # bind error handler
+    @app.before_request
+    def check_json_body():
+        # make API request inputs compulsory having `application/json` mimetype
+        methods_require_json = ('POST', 'PUT', 'PATCH')
+        if request.method in methods_require_json and not request.is_json:
+            raise ParseError(message='Body should be a JSON object')
+
+    # bind error handler for `Flask` routes
+    # needed for `auth` blueprint
     @app.errorhandler(ApiException)
     def handle_api_exception(e):
         return make_response(jsonify(e.to_dict()), e.error_status_code)
